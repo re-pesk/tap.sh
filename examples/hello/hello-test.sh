11c43375061f7226be31dca4d37a885840a27975
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2025-08-09 04:07:23 +0300

    Fix output of examples

--- examples/hello/hello-test.sh
+++ examples/hello/hello-test.sh
@@ -15,20 +15,21 @@ test_1() {
 	# test #1 does hello() print the expected output?
 	hello_out=$(hello)
 	tap_cmp "$hello_out" "Hello, World!" "hello"
+	echo
 }
 
 test_2() {
 	# test #2 does hello "you" print the expected output?
 	hello_out=$(hello "you")
 	tap_cmp "$hello_out" "Hello, you!" "hello 'you'"
+	echo
 }
 
 tests() {
 	test_1
 	test_2
-	echo
 	# print the test plan
-	tap_end 2
+	log_text "$(tap_end 2)\n"
 }
 
 tests
